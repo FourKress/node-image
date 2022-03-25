cf8c151fb537884680a6fac93258d48504547f19
吴东 <511970747@qq.com>
2022-03-25 23:53:51 +0800

    chore: 更新token

--- src/wechaty/bot.ts
+++ src/wechaty/bot.ts
@@ -14,7 +14,7 @@ const timer = Date.now();
 const bot = new Wechaty({
   name: '动起手来',
   puppet: new PuppetPadlocal({
-    token: 'puppet_padlocal_482484bc4a244531a2d692f1a17bbcef',
+    token: 'puppet_padlocal_b5187f20220b48969e2a91c9c68a85db',
   }),
 });
 
