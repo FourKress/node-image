2eeaf3111b0ed9dc8f9daa91c7068b352050a784
吴东 <511970747@qq.com>
2022-04-28 10:28:39 +0800

    feat: 提现通知,增加失败信息展示

--- src/wechaty/wechaty.service.ts
+++ src/wechaty/wechaty.service.ts
@@ -164,7 +164,7 @@ export class WechatyService {
     const { phoneNum, withdrawAmt, withdrawStatus, errCodeDes } = user;
     await baseNotice(
       `"${user.nickName}"发起提现${withdrawStatus ? '成功' : '失败'}，\n${
-        !withdrawStatus && `失败原因: ${errCodeDes}，\n`
+        withdrawStatus ? '' : `失败原因: ${errCodeDes}，\n`
       }提现金额: ${withdrawAmt}，\n联系电话：${phoneNum}，请知悉。`,
     );
   }
