807785da542491ab778cb680fece79796045e95d
wudong <51970747@qq.com>
2022-07-01 22:05:25 +0800

    feat: pm2自动化

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -17,34 +17,34 @@ export class BotService {
       const exec = shell.exec;
       const echo = shell.echo;
       const exit = shell.exit;
-      if (exec('git checkout bot').code !== 0) {
-        echo('Error git checkout branch failed');
-        exit(1);
-      }
+      // if (exec('git checkout bot').code !== 0) {
+      //   echo('Error git checkout branch failed');
+      //   exit(1);
+      // }
+      //
+      // if (exec('git pull').code !== 0) {
+      //   echo('Error git pull failed');
+      //   exit(1);
+      // }
 
-      if (exec('git pull').code !== 0) {
-        echo('Error git pull failed');
-        exit(1);
-      }
+      // const file = fs.readFileSync(filePath, 'utf-8');
+      // const newFile = file.replace(
+      //   /puppet_padlocal_[\d\w]+/,
+      //   `puppet_padlocal_${token}`,
+      // );
+      //
+      // // 同步写入内容
+      // fs.writeFileSync(filePath, newFile, 'utf8');
 
-      const file = fs.readFileSync(filePath, 'utf-8');
-      const newFile = file.replace(
-        /puppet_padlocal_[\d\w]+/,
-        `puppet_padlocal_${token}`,
-      );
-
-      // 同步写入内容
-      fs.writeFileSync(filePath, newFile, 'utf8');
-
-      if (exec('git add .').code !== 0) {
-        echo('Error git add failed');
-        exit(1);
-      }
-
-      if (exec(`git commit -m '更换token'`).code !== 0) {
-        echo('Error git commit failed');
-        exit(1);
-      }
+      // if (exec('git add .').code !== 0) {
+      //   echo('Error git add failed');
+      //   exit(1);
+      // }
+      //
+      // if (exec(`git commit -m '更换token'`).code !== 0) {
+      //   echo('Error git commit failed');
+      //   exit(1);
+      // }
 
       // if (exec('git push').code !== 0) {
       //   echo('Error git push failed');
