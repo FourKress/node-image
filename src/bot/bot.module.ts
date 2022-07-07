e8876b0a1ebdc8f031e293319b16eb9829180b73
wudong <51970747@qq.com>
2022-07-07 22:46:47 +0800

    feat: 机器人自动化

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { BotController } from './bot.controller';
 import { BotService } from './bot.service';
 import { WechatyBot } from './wechatyBot';
 
 @Module({
+  imports: [HttpModule],
   controllers: [BotController],
   providers: [BotService, WechatyBot],
   exports: [WechatyBot],
