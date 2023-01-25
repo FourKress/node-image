1cfb991e5549298701461a9b8ce575de3841e9b0
吴东 <511970747@qq.com>
2023-01-25 10:21:42 +0800

    chore: 更换token

--- src/bot/wechatyBot.ts
+++ src/bot/wechatyBot.ts
@@ -22,7 +22,7 @@ export class WechatyBot {
   private readonly bot = new Wechaty({
     name: '动起手来',
     puppet: new PuppetPadlocal({
-      token: 'puppet_padlocal_0135d9333f1048309e1b2c49103ab83b',
+      token: 'puppet_padlocal_da95d7ae648c4b53ac449c934a61876c',
     }),
   });
 
